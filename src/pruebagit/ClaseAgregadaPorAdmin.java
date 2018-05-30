b622325b9a7925ff2353ef81cc0306b53ab47c00
Nolasco <Nolasco@192.168.8.155>
2018-05-30 10:04:14 -0600

    Proyecto original.

---
+++ src/pruebagit/ClaseAgregadaPorAdmin.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebagit;
+
+/**
+ *
+ * @author Nolasco
+ */
+public class ClaseAgregadaPorAdmin {
+    private String texto;
+
+    public ClaseAgregadaPorAdmin(String texto) {
+        this.texto = texto;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+    
+    
+    
+}
